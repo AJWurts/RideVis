dd600147aa1816d7052291eb2abf4109a029e614
Alex Wurts <ajwurts@gmail.com>
2020-06-17 12:33:40 -0400

    Uploaded most recent activies and parsed them

--- preprocessing/GetActivities.js
+++ preprocessing/GetActivities.js
@@ -1,37 +1,48 @@
-const axios = require('axios');
-var fs = require('fs');
+const axios = require("axios");
+var fs = require("fs");
 
-
-
-const userToken = '8af735ac30e1ef4e56af8769053aaf946b2f5094';
+const userToken = "eb75380c745260f357c24dbe26e9f7b32c0ac3cb";
 function getAllUserActivities(other, pageNumber, callback) {
-    let userID = '11540989';
-    let perPage = 30
-    axios.get('https://strava.com/api/v3/athlete/activities?page=' + pageNumber + '&per_page=' + perPage, { headers: { Authorization: "Bearer " + userToken } })
-        .then(result => {
+    let userID = "11540989";
+    let perPage = 30;
+    axios
+        .get(
+            "https://www.strava.com/api/v3/athlete/activities?page=" +
+                pageNumber +
+                "&per_page=" +
+                perPage,
+            { headers: { Authorization: "Bearer " + userToken } }
+        )
+        .then((result) => {
             let activities = result.data;
-            console.log(pageNumber, activities.length)
+            console.log(pageNumber, activities.length);
 
             // If amount of results is equal to perPage look for more
             if (activities.length === perPage) {
-                getAllUserActivities(activities.concat(other), pageNumber + 1, callback);
+                getAllUserActivities(
+                    activities.concat(other),
+                    pageNumber + 1,
+                    callback
+                );
             } else {
-                callback(activities.concat(other))
+                callback(activities.concat(other));
             }
         })
-        .catch(error => {
-            console.log(error)
+        .catch((error) => {
+            console.log(error);
+            console.log(error.response.data.errors);
             callback(other);
-        })
+        });
 }
 
-
-var allActivities = []
+var allActivities = [];
 getAllUserActivities([], 1, (activities) => {
     console.log(activities);
-    fs.writeFile('activities.json', JSON.stringify(activities), function (err, data) {
+    fs.writeFile("activities.json", JSON.stringify(activities), function (
+        err,
+        data
+    ) {
         if (err) console.log(err);
         console.log("Successfully Written to File.");
     });
-})
-
+});
